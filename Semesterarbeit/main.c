12af2db6da79392cd29e8129f05e9c966610d6ff
Michael Wettstein <chischte@gmx.ch>
2019-10-24 18:29:05 +0400

    Add main.c of Semesterarbeit

---
+++ Semesterarbeit/main.c
@@ -0,0 +1,18 @@
+/*
+ * Semesterarbeit.c
+ *
+ * Created: 24/10/2019 18:28:41
+ * Author : realslimshady
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

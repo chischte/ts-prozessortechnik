9460c27ad26e41a6121ef1773b12b4f5736afdb6
Michael Wettstein <chischte@gmx.ch>
2019-11-08 21:12:34 +0100

    Initial commit of the newly generated Calculator project

---
+++ Calculator/main.c
@@ -0,0 +1,26 @@
+/*
+ * *****************************************************************************
+ * CALCULATOR
+ * *****************************************************************************
+ * A simple program in C to add a calculator functionality to a prototyping
+ * board "EduBoard" as part of a school work.
+ * The code is written to be uploaded to an ATxmega128A3U AVR Microcontroller.
+ * The User interface consits of 4 push buttons and a 4x21 digits display. 
+ * *****************************************************************************
+ * Michael Wettstein
+ * November 2019, Zürich
+ * *****************************************************************************
+ * TODO:
+
+ */
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
